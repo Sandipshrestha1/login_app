f878b54b158d8c2c41d858c4b9ce9842aa8ead33
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-03-07 18:13:42 +0545

    Added bloc

---
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -0,0 +1,61 @@
+import 'dart:developer';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/user_repository.dart';
+
+class FirebaseUserRepo implements UserRepository {
+  final FirebaseAuth _firebaseAuth;
+
+  final usersCollection = FirebaseFirestore.instance.collection('user');
+
+  FirebaseUserRepo({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+
+// when user is sign-in and sign out . This method is triggered
+  @override
+  Stream<User?> get user {
+    return _firebaseAuth.authStateChanges().map((firebaseUser) {
+      return firebaseUser;
+    });
+  }
+
+// creating signIn method
+
+  @override
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+// creating signUp method
+  @override
+  Future<MyUser> signUp(MyUser myUser, String password) async {
+    try {
+      UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: myUser.email, password: password);
+      myUser = myUser.copyWith(userId: user.user!.uid);
+      return myUser;
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> setUserData(MyUser myUser) async {
+    try {
+      await usersCollection
+          .doc(myUser.userId)
+          .set(myUser.toEntity().toDocuments());
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+}
